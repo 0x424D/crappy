061b12f501b51d66e893eb7aa16bce3757a2d744
0x424D <30124318+0x424D@users.noreply.github.com>
2019-02-22 00:41:54 +0000

    Hey it's ACTUALLY a merge sort now

--- msort/src/main.py
+++ msort/src/main.py
@@ -1,28 +1,43 @@
-def split(L):
-	ret = []
-	for i in L:
-		ret.append([i])
-	while len(ret) > 1:
-		ret[0] = merge(ret[0], ret[1])
-		del ret[1]
-	return ret[0]
-	
+import random
+
 def merge(L1, L2):
-	for i in L2:
-		for n, j in enumerate(L1):
-			if i <= j:
-				L1.insert(n, i)
-				break
+	ret = []
+	
+	while L1 and L2:
+		if L1[0] <= L2[0]:
+			ret.append(L1[0])
+			L1 = L1[1:]
 		else:
-			L1.append(i)
-	return L1
+			ret.append(L2[0])
+			L2 = L2[1:]
 	
+	while L1:
+		ret.append(L1[0])
+		L1 = L1[1:]
+	while L2:
+		ret.append(L2[0])
+		L2 = L2[1:]
+	
+	return ret
+	
+def msort(L):	
+	# splitting
+	tmp = [[i] for i in L]
+	
+	# merging
+	ret = [[]]
+	while tmp:
+		ret[0] = merge(ret[0], tmp[0])
+		tmp = tmp[1:]
+	
+	# we end up with a list of one list, of the form [[a, b, c,...]] where a, b, c,... are integers
+	# therefore, we return the first element of ret to avoid the structure being nested
+	return ret[0]
+
 def main():
-	from random import randint
-	L = [randint(0, 999) for i in range(20)]
-	
-	print(f"L (unsorted): {L}")
-	print(f"L (sorted): {split(L)}")
+	# let's run the sort on a list that consists of a random amount of random integers between -1000 and 1000 (inclusive)
+	L = [random.randint(-1000, 1000) for i in range(random.randint(0, 100))]
+	print(msort(L))
 	
 if __name__ == "__main__":
 	main()
