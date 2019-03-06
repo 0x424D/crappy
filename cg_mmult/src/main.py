2baf0a913d257522dc21b1870c35111a55c1bb60
0x424D <30124318+0x424D@users.noreply.github.com>
2019-03-06 17:48:43 +0000

    Update main.py

--- cg_mmult/src/main.py
+++ cg_mmult/src/main.py
@@ -1,7 +1,1 @@
-M = [[1, 4],
-	[1, 3]]
-
-N = [[4, 3],
-	[0, -1]]
-
-l=range;m=len;r=[([sum([M[i][k]*N[k][j]for k in l(m(N))])for j in l(m(N[0]))])for i in l(m(M))];print(r)
+M=[[1,2]];N=[[3],[4]];l=range;m=len;r=[([sum([M[i][k]*N[k][j]for k in l(m(N))])for j in l(m(N[0]))])for i in l(m(M))];print(r)
