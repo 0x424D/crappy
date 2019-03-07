c3b51553e361768845fdd519002b80c5d6f270d9
0x424D <30124318+0x424D@users.noreply.github.com>
2019-03-07 00:32:17 +0000

    Significant shrinkage (126 -> 107)

--- cg_mmult/src/main.py
+++ cg_mmult/src/main.py
@@ -1,1 +1,1 @@
-M=[[1,2]];N=[[3],[4]];l=range;m=len;r=[([sum([M[i][k]*N[k][j]for k in l(m(N))])for j in l(m(N[0]))])for i in l(m(M))];print(r)
+M=[[1,2]];N=[[3],[4]];print([[sum(i[k]*N[k][j]for k in range(len(N)))for j in range(len(N[0]))]for i in M])
